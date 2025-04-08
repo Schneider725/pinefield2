cae5fe9822af09205054f0a14816080eee11b1bd
stephen etienne <sschneider725@gmail.com>
2025-04-08 12:47:01 -0400

    routemap implementation

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,28 @@ import Dijon from "./pages/Dijon.jsx"
 import './index.css'
 import Navbar from "./Navbar.jsx";
 function App(){
-    let Component
-    switch(window.location.pathname){
-        case"/":
-            Component = Field()
-            break
-        case"/pinegrove":
-            Component = Pine()
-            break
-        case"/fieldmedic":
-            Component = Field()
-            break
-        case"/dijon":
-            Component = Dijon()
+    // let Component
+    // switch(window.location.pathname){
+    //     case"/":
+    //         Component = Field()
+    //         break
+    //     case"/pinegrove":
+    //         Component = Pine()
+    //         break
+    //     case"/fieldmedic":
+    //         Component = Field()
+    //         break
+    //     case"/dijon":
+    //         Component = Dijon()
+    //         break
+    const routeMap = {
+        "/" : Field,
+        "/pinegrove" : Pine,
+        "/fieldmedic" : Field,
+        "/dijon" : Dijon
     }
+    const Component = routeMap(window.location.pathname) || Field;
+
     return(
         <>
             <div className={"container"}>
