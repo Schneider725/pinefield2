66be90370f4c1fbf5dd20ba83191b0a80e57f67a
stephen etienne <sschneider725@gmail.com>
2025-04-08 12:50:29 -0400

    updated routemap

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,13 @@ import Dijon from "./pages/Dijon.jsx"
 import './index.css'
 import Navbar from "./Navbar.jsx";
 function App(){
-    // let Component
-    // switch(window.location.pathname){
-    //     case"/":
-    //         Component = Field()
-    //         break
-    //     case"/pinegrove":
-    //         Component = Pine()
-    //         break
-    //     case"/fieldmedic":
-    //         Component = Field()
-    //         break
-    //     case"/dijon":
-    //         Component = Dijon()
-    //         break
     const routeMap = {
-        "/" : Field,
-        "/pinegrove" : Pine,
-        "/fieldmedic" : Field,
-        "/dijon" : Dijon
+        "/" : Field(),
+        "/pinegrove" : Pine(),
+        "/fieldmedic" : Field(),
+        "/dijon" : Dijon()
     }
-    const Component = routeMap(window.location.pathname) || Field;
+    const Component = routeMap[window.location.pathname] || Field;
 
     return(
         <>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar(){
     return(
         <div className={"nav"}>
-            <a href={"home"} className={"title"}>Disco</a>
+            <a href={"/"} className={"title"}>Disco</a>
             <ul>
                 <li><a href={"/pinegrove"}>pinegrove</a></li>
                 <li><a href={"/fieldmedic"}>field medic</a></li>
