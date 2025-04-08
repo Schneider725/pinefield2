be1cd82bf1e296566383a8ba03fe06ecd44e00b0
stephen etienne <sschneider725@gmail.com>
2025-04-08 13:34:29 -0400

    burger menu

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,30 @@
-function Navbar(){
-    return(
-        <div className={"nav"}>
-            <a href={"/"} className={"title"}>Disco</a>
-            <ul>
-                <li><a href={"/pinegrove"}>pinegrove</a></li>
-                <li><a href={"/fieldmedic"}>field medic</a></li>
-                <li><a href={"/dijon"}>dijon</a></li>
-            </ul>
-        </div>
-    )
+import { useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <div className="nav">
+                <div className="title">Disco</div>
+                <button className="burger" onClick={() => setIsOpen(true)}>☰</button>
+            </div>
+
+            {isOpen && (
+                <div className="modal" onClick={() => setIsOpen(false)}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="close-btn" onClick={() => setIsOpen(false)}>✕</button>
+                        <ul>
+                            <li><a href="/pinegrove" onClick={() => setIsOpen(false)}>Pinegrove</a></li>
+                            <li><a href="/fieldmedic" onClick={() => setIsOpen(false)}>Field Medic</a></li>
+                            <li><a href="/dijon" onClick={() => setIsOpen(false)}>Dijon</a></li>
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
-export default Navbar
+
+export default Navbar;
